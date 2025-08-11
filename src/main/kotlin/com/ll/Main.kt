5a1c7096320c2ab802b05bdc9ac3043f9ed911db
jhs512 <jangka512@gmail.com>
2025-08-11 15:35:12 +0900

    work

--- src/main/kotlin/com/ll/Main.kt
+++ src/main/kotlin/com/ll/Main.kt
@@ -1,7 +1,13 @@
 package com.ll
 
 fun main() {
-    for (i in 1..5) {
-        println("i = $i")
+    println("== 명언 앱 ==")
+
+    while (true) {
+        print("명령) ")
+        val input = readlnOrNull()
+
+        if (input == "종료")
+            break
     }
 }
